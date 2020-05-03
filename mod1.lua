aabc76ea81c0545421e3f837d8ef25c51d941442
ihatelua <54477695+ihatelua@users.noreply.github.com>
2020-05-04 00:02:50 +0900

    Update mod1.lua

--- mod1.lua
+++ mod1.lua
@@ -79,16 +79,3 @@ function Slow_Peer(id)
 		peer:send("start_timespeed_effect", "pause", "pausable", "player;game;game_animation", 0.05, 1, 3600, 1)
 	end
 end
-if managers.network._session then
-	local menu_options = {}
-	for _, peer in pairs(managers.network:session():peers()) do
-		if peer:rank() and peer:level() then
-			menu_options[#menu_options+1] ={text = "(" .. peer:user_id() .. ") " .. peer:name(), data = peer:id(), callback = Slow_Peer}
-		else
-			menu_options[#menu_options+1] ={text = peer:name(), data = peer:id(), callback = Slow_Peer}
-		end
-	end
-	menu_options[#menu_options+1] = {text = "Return", is_cancel_button = true}
-	local menu = QuickMenu:new("Slow-mo", "Select who gets slow", menu_options)
-	menu:Show()
-end
